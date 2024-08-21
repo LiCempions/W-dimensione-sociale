6dd83a9b70417835530fe09bc831c6871cd2caa2
LiCempions <nuvoletti.popo@gmail.com>
2024-08-21 15:29:34 +0200

    function to highlight tags in text

--- frontend/global.js
+++ frontend/global.js
@@ -39,3 +39,59 @@ function setSParam(param /*String*/, user) {
     params.set(param, user);
     window.location.search = params.toString();
 }
+
+function firstOccurrence(string, searchStrings/* array */, startPos) {
+    // Thank you JS for not letting regexes in indexOf
+    let indices = [];
+    let found;
+    let smallest;
+
+    // Find the first occurrence of each searchString starting from startPos
+    for (const curr of searchStrings) {
+        found = string.indexOf(curr, startPos);
+        if (found == -1) {found = string.length};
+        indices.push(found);
+    }
+    
+    // Select the firstest occurence and return it
+    smallest = indices[0];
+    for (const curr of indices) {
+        if (curr < smallest) {smallest = curr};
+    }
+
+    return smallest;
+}
+
+function highlightTags(text/*String*/) {
+    let segments = [];
+    const stopChars = ' \n.,;:!?';
+    let stop = 0;
+    let start = firstOccurrence(text, ['#','@'], stop);
+    const sParams = new URLSearchParams(window.location.search);
+    const tagTypes = { '@':"user", '#':"hashtag" }
+
+    if (start == text.length) { return text };
+    
+    while (stop < text.length){
+        segments.push( text.slice(stop, start) );
+        
+        stop = firstOccurrence(text, stopChars, start);
+        
+        segments.push( text.slice(start, stop) );
+        
+        start = firstOccurrence(text, ['#','@'], stop);
+    }
+    
+    
+    for (let i = 1; i < segments.length; i=i+2) {
+        const tagType = tagTypes[ segments[i][0] ]
+
+        sParams.set( tagType, segments[i].slice(1) );
+        
+        segments[i] = `<a class='tag' href='frontend/homepage.html?${sParams.toString()}'>${segments[i]}</a>`;
+
+        sParams.delete(tagType);
+    }
+    
+    return segments.join("");
+}
