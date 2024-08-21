db7f51e5c4c340765e1a2431849788a0121da354
LiCempions <nuvoletti.popo@gmail.com>
2024-08-21 16:25:26 +0200

    lowercase u

--- frontend/indexScripts.js
+++ frontend/indexScripts.js
@@ -9,7 +9,7 @@ document.getElementById("register").addEventListener("submit", async (event)=>{
 
     if (formData.username.search(illegalChars)!=-1) {
         document.getElementById("userRegLabel").innerHTML =
-        `Nome Utente - <span class="text-danger">proibiti: ${
+        `Nome utente - <span class="text-danger">proibiti: ${
             illegalChars.source.slice(1, -1).replace(" ","").match(/./g).join(' ').concat(" e spazi")
             // mostra i singoli caratteri della regex separati da spazi bianchi
             // sostituisce lo spazio bianco nella regex con la scritta "e spazi"
