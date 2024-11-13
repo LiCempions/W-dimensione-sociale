3cdb5779f41b9e4da29e05708697ee24a94ffe9e
LiCempions <nuvoletti.popo@gmail.com>
2024-11-13 21:57:27 +0100

    moved data types to new file

--- backend/main.py
+++ backend/main.py
@@ -28,41 +28,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# ======================================
-# ---------- Inizializzazione ----------
-# ======================================
-class userRegister(BaseModel):
-    username: str
-    email: str
-    password: str
-
-class userCredentials(BaseModel):
-    username: str
-    password: str
-
-class post(BaseModel):
-    username: str
-    postText: str
-
-class answer(BaseModel):
-    username:str
-    postId:str
-    answerText:str
-
-class message(BaseModel):
-    userAuth: str
-    userDest: str
-    DMText: str
-
-class likeDataPut(BaseModel):
-    username: str
-    postId: str
-
-class doubleUser(BaseModel):
-    username1: str
-    username2: str
-
-
 # =========================================
 # ----------------- Rotte -----------------
 # =========================================
