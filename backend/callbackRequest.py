48509096da8c53b73a04a2177b19dab648d0a917
LiCempions <nuvoletti.popo@gmail.com>
2025-03-05 15:39:09 +0100

    procedureName instead of successMsg

--- backend/callbackRequest.py
+++ backend/callbackRequest.py
@@ -2,7 +2,8 @@ import mysql.connector
 import mysql.connector.cursor
 from types import FunctionType
 
-def callbackRequest(query:str, values:tuple|None, config:dict, cursorProcessor:FunctionType|None, successMsg:str="Richiesta eseguita con successo"):
+def callbackRequest(query:str, values:tuple|None, config:dict, cursorProcessor:FunctionType|None, procedureName:str|None=None):
+    procedureName = procedureName or cursorProcessor.__name__ or "callback generico"
     try:
         conn = mysql.connector.connect(**config)
         cursor = conn.cursor()
@@ -11,9 +12,9 @@ def callbackRequest(query:str, values:tuple|None, config:dict, cursorProcessor:F
             return cursorProcessor(cursor)
         else:
             conn.commit()
-            return {"msg": successMsg}
+            return {"msg": f"Procedura \"{procedureName}\" eseguita con successo"}
     except mysql.connector.Error as err:
-        return {"msg": f'Errore durante la procedura "{cursorProcessor.__name__}": {err}'}
+        return {"msg": f'Errore durante la procedura "{procedureName}": {err}'}
     finally:
         cursor.close()
         conn.close()
